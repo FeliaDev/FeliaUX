e15abba74f7f69e819ba75ad08e4eab1e1349098
Felia <dpinoagustin@gmail.com>
2020-04-25 02:00:29 -0300

    Ambiente de Testing

--- js/loadWidgets.js
+++ js/loadWidgets.js
@@ -1,62 +1,57 @@
-const mbMenu = new Menu({"master":"ParentBody", "id":"MainMenuBar", "class":""});
+const frmMainFrame = new Frame({"master":"MainHeader", "id":"MyFrame", "class":""})
+const lblLabel = new Label({"master":frmMainFrame.getId(), "id":"MyLabel", "class":""}, "Label");
+const inpInput = new Input({"master":frmMainFrame.getId(), "id":"MyInput", "class":""}, "I'm an Input");
+const btnButton = new Button({"master":frmMainFrame.getId(), "id":"MyButton", "class":""}, "Button", ()=>{console.log(inpInput.getContent())});
+const rdbRadioButton = new RadioButton({"master":frmMainFrame.getId(), "id":"MyRadioButton", "class":""});
+const chkCheckbox = new Checkbox({"master":frmMainFrame.getId(), "id":"MyCheckbox","class":""})
+const txtTextarea = new Text({"master":frmMainFrame.getId(), "id":"MyText", "class":""}, 200, 200, "Hello I'm a Text Area.")
+const lstListbox = new ListBox({"master":frmMainFrame.getId(), "id":"MyListbox", "class":""}, 3, 200, 45);
+const cmbComboBox = new ComboBox({"master":frmMainFrame.getId(), "id":"MyComboBox", "class":""}, 200, 20);
+const inumInputNumber = new InputNumber({"master":frmMainFrame.getId(), "id":"MyInputNum", "class":""});
+const pswInputPsw = new InputPassword({"master":frmMainFrame.getId(), "id":"MyInputPass", "class":""});
+const winTopWin = new TopWindow("MyWindow", "", "I'm a Top Window", 200, 200, 100, 100)
 
-const msMenuFile = new MenuSection({"master":mbMenu.getId(), "id":"FileMenu", "class":""}, "File");
-const msMenuEdit = new MenuSection({"master":mbMenu.getId(), "id":"FileEdit", "class":""}, "Edit");
-const msMenuHelp = new MenuSection({"master":mbMenu.getId(), "id":"FileHelp", "class":""}, "Help");
+const lblLabelTopWin = new Label({"master":winTopWin.getId(), "id":"MyLabelIntoWin", "class":""}, "I'm a Label living in TopWindow")
 
+const mbrMenuBar = new Menu({"master":"MainHeader", "id":"MyMenuBar", "class":""});
+const mbtnFileMenu = new MenuSection({"master":mbrMenuBar.getId(), "id":"MyFileMenu", "class":""}, "File");
+const mbtnEditMenu = new MenuSection({"master":mbrMenuBar.getId(), "id":"MyEditMenu", "class":""}, "Edit");
 
-function newFile(){
-    txtTextBox.pack()
-}
+mbtnFileMenu.addItem("New File", ()=>{console.log("new file clicked")});
+mbtnFileMenu.addItem("Open File", ()=>{console.log("open file clicked")});
+mbtnFileMenu.addItem("Save File", ()=>{console.log("Save file clicked")});
+mbtnFileMenu.addItem("Save As File", ()=>{console.log("Save As file clicked")});
+mbtnFileMenu.addItem("Exit", ()=>{console.log("exit clicked")});
+mbtnFileMenu.addItem("Exita", ()=>{console.log("exit clicked")});
 
-function openFile(){
-    
-    function openFileDialog () {  // this function must be called from  a user
-        // activation event (ie an onclick event)
-        console.log("Hola")
+mbtnEditMenu.addItem("Undo", ()=>{console.log("undo clicked")})
+mbtnEditMenu.addItem("Redo", ()=>{console.log("redo clicked")})
 
-        // Create an input element
-        var inputElement = document.createElement("input");
+let aElementsList = ["I", "am", "a", "Listbox"];
+let aElementsCombo = ["I", "am", "a", "ComboBox"];
 
-        // Set its type to file
-        inputElement.type = "file";
-
-        // dispatch a click event to open the file dialog
-        inputElement.dispatchEvent(new MouseEvent("click")); 
-    }
-    let winAskForFile = new TopWindow("askForFile", "", "Open File from Desktop", 300, 100, 300, 300);
-    let inpPath = new Input({"master": winAskForFile.getId(), "id":"InpPath", "class":""});
-    let btnOpenFromDesktop = new Button({"master": winAskForFile.getId(), "id":"BtnOpenFile", "class":""}, "Browse", openFileDialog);
-    winAskForFile.pack();
-    inpPath.pack();
-    btnOpenFromDesktop.pack();
-}
-
-msMenuFile.addItem("New File", newFile);
-msMenuFile.addItem("Open File", openFile);
-msMenuFile.addItem("Save",()=>{say("Save")});
-msMenuFile.addItem("Save As",()=>{say("Save As")});
-msMenuFile.addItem("Exit",()=>{say("Exit")});
-
-msMenuEdit.addItem("Redo",()=>{});
-msMenuEdit.addItem("Undo",()=>{});
-msMenuEdit.addItem("Cut",()=>{});
-msMenuEdit.addItem("Copy",()=>{});
-msMenuEdit.addItem("Paste",()=>{});
-
-msMenuHelp.addItem("Welcome",()=>{});
-msMenuHelp.addItem("Docs",()=>{});
-msMenuHelp.addItem("Tutorial",()=>{});
-msMenuHelp.addItem("About",()=>{});
-msMenuHelp.addItem("Credits",()=>{});
-
-const frmMainFrame = new Frame({"master":"ParentBody", "id":"MainFrame", "class":""}, 1350, 655-38);
-const txtTextBox = new Text({"master": frmMainFrame.getId(), "id":"MainTextBox", "class":""});
+lblLabel.setText("I'm a Label");
+btnButton.setText("I'm a Button");
+lstListbox.setElementsList(aElementsList);
+cmbComboBox.setElementsList(aElementsCombo);
+inumInputNumber.setMinRange(0);
 
 
-mbMenu.pack();
-msMenuFile.pack();
-msMenuEdit.pack();
-msMenuHelp.pack();
-
+mbrMenuBar.pack();
+mbtnFileMenu.pack();
+mbtnEditMenu.pack();
 frmMainFrame.pack();
+lblLabel.pack();
+btnButton.pack();
+inpInput.pack();
+rdbRadioButton.pack();
+chkCheckbox.pack();
+txtTextarea.pack();
+lstListbox.pack();
+cmbComboBox.pack();
+inumInputNumber.pack();
+pswInputPsw.pack();
+
+winTopWin.pack();
+
+lblLabelTopWin.pack()
